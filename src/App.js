5fe277537c73e15afe50394c05116527dfa8da9c
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-09-07 18:44:50 -0300

    formulário login

--- src/App.js
+++ src/App.js
@@ -10,7 +10,15 @@ function App() {
       </section>
       <section className="secFormulario">
         <div className="formulario">
-
+          <h1>Login</h1>
+          <form>
+            <input placeholder="CPF"></input>
+            <input placeholder="Senha"></input>
+            <button className="entrar">Entrar</button>
+            Esqueceu a senha?
+            <span></span>
+            <button className="cadastrar">Cadastrar</button>
+          </form>
         </div>
       </section>
     </main>
